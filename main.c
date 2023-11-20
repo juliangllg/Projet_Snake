edfd4b32fb8015d3b06cd3c64d6d7dde464d7321
gallego <julian.gallego180105@gmail.com>
2023-11-20 14:58:55 +0100

    début

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 #include <graph.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define TAILLE_CARRE 10
+#define TAILLE_CARRE 20
 
 void main(void){
     int i = 0;
     int j = 0;
-    int x = 10;
-    int y = 10; 
+    int x = 20;
+    int y = 20; 
     InitialiserGraphique();
-    CreerFenetre(20,20,620,430);
+    CreerFenetre(20,20,1240,950);
+    EffacerEcran(CouleurParComposante(255,255,255));
+    DessinerRectangle(20,20,1200,800)
     for (i; i<40; i++){
         for(j; j< 60; j++){
             DessinerRectangle(x,y,TAILLE_CARRE, TAILLE_CARRE);
-            x+=10;
+            
+            x+=20;
         }
-        x = 10;
-        y += 10;
+        x = 20;
+        y += 20;
         j=0;
     }
     Touche();
