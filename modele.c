523e1eda3dab8309a6b5957969c3b6dfd638e810
izanic <izanic@localhost>
2023-11-20 21:20:10 +0100

    Création des structure Serpent,Jeu et pommes

---
+++ modele.c
@@ -0,0 +1,14 @@
+struct Jeu{
+	short int total_point,temps;
+	unsigned char en_pause;
+};
+	
+struct Serpent {
+	short int taille;
+	float vitesse;
+	unsigned char en_vie_bool;
+};
+
+struct Pomme {
+	char points,couleur,x,y;
+};
